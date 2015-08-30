d2ece352fc09ac07f22b9b17ff97266e17c669e6
DMCristallo <dfm06150@vtc.vsc.edu>
2015-08-29 21:52:44 -0400

    Added command line support.
    3 command line commands added

--- Main.py
+++ Main.py
@@ -2,28 +2,40 @@ import avail
 import index
 import search
 import os
+import sys
 
 print "Drives detected: %s" % (avail.get_available_drives())
 
 loc = avail.get_available_drives()
 loc2= avail.get_available_drives()
 
-choose = raw_input("Full index or single Drive?: ").lower()
-if choose == 'exit': quit()
-
-#add in some timeout thing so a update can be run automaticly
-
-if os.path.exists("D:\\index\\index") == True:
-    os.remove("D:\\index\\index")
-elif os.path.exists("D:\\index") == False:
+if os.path.exists("D:\\index") == False:
     os.mkdir("D:\\index")
+
+if len(sys.argv)>=2: #command line options
+    arg1 = sys.argv[1]
+    if arg1=='s':
+        search.search()
+        quit()
+    if arg1=='f':
+        index.all(loc,loc2)
+    if arg1=='d' and len(sys.argv)>=3:
+        index.drive(sys.argv[2])
+        print "Complete"
+        quit()
+
+    #maybe add clean up for instances run using commands.
+
+choose = raw_input("Full index or single Drive?: ").lower()
+if choose == 'exit':
+    quit()
 
 if choose in ['s', 'single', 'o', 'one']:
     cloc = raw_input("What drive? eg C:\\ : ") #maybe get to handle C ect
     if cloc == 'exit': quit() #remove later
     print "Drive: %s selected" % (cloc)
     print "Rebuilding Index files. This may take several minutes.... " #maybe add progress ellipses
-    index.drive(cloc) #who needs error handling.
+    index.drive(cloc) #now with (some) error checking
 
 elif choose in ['f', 'full','a', 'all']:
     print "Checking drives %s" % loc
@@ -34,7 +46,7 @@ else:
 
 print "Complete"
 
-#add option to search without rebuilding, stand alone
+#done if in command line #add option to search without rebuilding, stand alone
 
 #expand to more than just name search maybe stat()
 more= raw_input("\nSearch for something? ").lower()
--- README.md
+++ README.md
@@ -6,6 +6,13 @@ Start main.
 The program will list all the drives connected to the computer and create D:\index if it doesnt exist.
 Follow instructuions, this will overwrite the last index made!
 
-search for file name or extention.
+Search for file name or extention with or without case sensitivity.
 
 List of currently available commands: tba
+Command line arguments:
+
+s --skips to search
+
+f --does a full index
+
+d X:\ --Indexs drive X
