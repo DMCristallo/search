0dd616695e8ef86afbe8fb21fdafd25771bd9a68
DMCristallo <dfm06150@vtc.vsc.edu>
2015-08-29 20:51:13 -0400

    Added Case options
    Search now Case sensitive / Insensitive

--- search.py
+++ search.py
@@ -2,12 +2,24 @@
 def search():
     search = open("D:\\index\\index", "r")
 
+    case= raw_input("Case sensitive? [Y]/[N] " ) #default case insensitive
+
     phrase = raw_input("Enter Search Query ")
     #phrase = "test"
-    print "Searching for %s" % (phrase)
+    print "Searching for %s \n" % (phrase)
 
-    for line in search:
-        if phrase in line:
-        #if phrase == line:
-            print line
+    if case in ['N', 'n']:
+        for line in search:
+            if phrase.lower() in line.lower():
+            #if phrase == line:
+                print line
+    else:
+        for line in search:
+            if phrase in line:
+            #if phrase == line:
+                print line
+
     search.close()
+
+    #hmm looks case sensitive
+    #maybe add search the search
